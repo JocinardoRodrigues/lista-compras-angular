4a8e88fdbb0f20ba8527d22a54641c2bc572f87b
Diogo Machado <diogomachado@saocamilo-es.br>
2013-11-09 14:39:52 -0200

    Suporte a localStorage

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,49 @@
 function ListaComprasController($scope) {
     
-    // Array inicial
-   $scope.itens = [
-        {produto: 'Leite', quantidade: 2, comprado: false},
-        {produto: 'Pão', quantidade: 10, comprado: false},
-        {produto: 'Frango', quantidade: 3, comprado: false},
-        {produto: 'Linguiça', quantidade: 2, comprado: false},
-        {produto: 'Sal grosso', quantidade: 1, comprado: false}
-    ];
+    // Verifica se tem uma lista armazenada
+    if (localStorage["itensArmazenados"]){
+
+        // Retorna a lista em formato array
+        var lista = JSON.parse(localStorage["itensArmazenados"]);
+
+    }else{
+
+
+        // Se não recebe um array vazio
+        var lista = [];
+
+    }
+
+    // Armazeno no local storage
+    localStorage["itensArmazenados"] = JSON.stringify(lista);
+
+    // Associo o array a uma "variavel" do angular
+   $scope.itens = lista;
  
     // Adiciona itens no array
     $scope.adicionaItem = function () {
+
+        // Adiciona no array do angular
         $scope.itens.push({produto: $scope.item.produto,
                            quantidade: $scope.item.quantidade,
                            comprado: false});
-        $scope.item.produto = $scope.item.quantidade = '';
+
+        // Não entendi
+        // $scope.item.produto = $scope.item.quantidade = '';
+
+        // Adiciona no array do localstorage
+        localStorage["itensArmazenados"] = angular.toJson($scope.itens);
+
     };
 
-    // Remove
+    // Remove itens do array
     $scope.removerItem = function(index){
+        
+        // Remove do array do angular
         $scope.itens.splice(index, 1);
+
+        // Armazena denovo
+        localStorage["itensArmazenados"] = angular.toJson($scope.itens);
+
     }
 }
